16a2cbe75f8e85557479e845901bebdeb3951934
Steven <steventancheta@gmail.com>
2016-09-30 16:19:16 -0700

    Added npm scripts and basic express server

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 8080;
+
+app.use('/', express.static('public'));
+
+app.listen(port, function() {
+   console.log('Now listening on port ' + port);
+});
